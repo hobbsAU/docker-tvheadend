77d0925058d3ad921b3c307c1a876fa29b90392a
hobbsau <adrianhobbs@gmail.com>
2016-02-14 16:45:17 +1100

    added run and stop scripts

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo docker rm tvheadend-service
+sudo docker rm tvheadend-data
+
+sudo docker pull hobbsau/tvheadend
+
+sudo docker create \
+ --name tvheadend-data \
+ -v /srv/tvheadend/config:/config \
+ -v /srv/tvheadend/recordings:/recordings \
+ hobbsau/tvheadend
+
+sudo docker run -d \                                                                                                    
+ --restart=always \
+ --net="host" \
+ --volumes-from tvheadend-data \
+ --name tvheadend-service \
+ hobbsau/tvheadend
+
